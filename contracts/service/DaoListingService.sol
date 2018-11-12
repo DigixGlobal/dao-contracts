4ea632036e3e84598077b77d46497e50afc709b1
Rohit Narurkar <rohitnarurkar@protonmail.com>
2018-11-12 12:18:11 +0800

    [18] duplicate code removed (Resolves DGDG-140)

--- contracts/service/DaoListingService.sol
+++ contracts/service/DaoListingService.sol
@@ -44,30 +44,6 @@ contract DaoListingService is
         _contract = DaoStorage(get_contract(CONTRACT_STORAGE_DAO));
     }
 
-    function daoWhitelistingStorage()
-        internal
-        constant
-        returns (DaoWhitelistingStorage _contract)
-    {
-        _contract = DaoWhitelistingStorage(get_contract(CONTRACT_STORAGE_DAO_WHITELISTING));
-    }
-
-    /**
-    @notice Check if a certain address is whitelisted to read sensitive information in the storage layer
-    @dev if the address is an account, it is allowed to read. If the address is a contract, it has to be in the whitelist
-    */
-    function isWhitelisted(address _address)
-        internal
-        constant
-        returns (bool _isWhitelisted)
-    {
-        uint size;
-        assembly {
-            size := extcodesize(_address)
-        }
-        _isWhitelisted = size == 0 || daoWhitelistingStorage().whitelist(_address);
-    }
-
     /**
     @notice function to list moderators
     @dev note that this list may include some additional entries that are
