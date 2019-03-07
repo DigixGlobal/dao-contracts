94e0239f66da4c5c7fe135f0ef1b48c69d178046
Rohit Narurkar <rohitnarurkar@protonmail.com>
2019-03-07 15:40:28 +0800

    [fix] fix gas price

--- scripts/mainnet-deploy/4_recover_leftover_funds.js
+++ scripts/mainnet-deploy/4_recover_leftover_funds.js
@@ -16,10 +16,10 @@ module.exports = async () => {
     assert.ok(!isInvalid(process.env.COLLECTOR), 'Please provide the address for COLLECTOR');
 
     const collector = process.env.COLLECTOR;
-    const gasPriceInGwei = parseInt(process.env.GAS_PRICE_IN_GWEI, 10);
+    const gasPriceInGwei = !isInvalid(process.env.GAS_PRICE_IN_GWEI) ?
+      parseInt(process.env.GAS_PRICE_IN_GWEI, 10) : 20;
 
-    const gasNeededForTransfer = !isInvalid(process.env.GAS_PRICE_IN_GWEI) ?
-      bN(21000 * 20 * (10 ** 9)) : bN(21000 * gasPriceInGwei * (10 ** 9));
+    const gasNeededForTransfer = bN(21000 * gasPriceInGwei * (10 ** 9));
 
     const collectEth = (sender) => {
       web3.eth.getBalance(sender, (e, balance) => {
@@ -28,6 +28,7 @@ module.exports = async () => {
           from: sender,
           to: collector,
           value: balance.minus(gasNeededForTransfer),
+          gasPrice: gasPriceInGwei * (10 ** 9),
         }, function (e, r) {
           if (e) {
             console.log('error : ', e);
