98ffe36bbfe2ee9397ebd4e50e11de9f7b156ba0
Rohit Narurkar <rohitnarurkar@protonmail.com>
2019-03-25 11:02:08 +0800

    deployed on kovan 0.0.7

--- contracts/mock/storage/MockDaoConfigsStorage.sol
+++ contracts/mock/storage/MockDaoConfigsStorage.sol
@@ -4,6 +4,16 @@ import "../../storage/DaoConfigsStorage.sol";
 
 contract MockDaoConfigsStorage is DaoConfigsStorage {
     constructor(address _resolver) public DaoConfigsStorage(_resolver) {
+      uintConfigs[CONFIG_LOCKING_PHASE_DURATION] = 4 hours;
+      uintConfigs[CONFIG_QUARTER_DURATION] = QUARTER_DURATION;
+      uintConfigs[CONFIG_VOTING_COMMIT_PHASE] = 10 minutes;
+      uintConfigs[CONFIG_VOTING_PHASE_TOTAL] = 20 minutes;
+      uintConfigs[CONFIG_INTERIM_COMMIT_PHASE] = 10 minutes;
+      uintConfigs[CONFIG_INTERIM_PHASE_TOTAL] = 20 minutes;
+      uintConfigs[CONFIG_SPECIAL_PROPOSAL_COMMIT_PHASE] = 10 minutes;
+      uintConfigs[CONFIG_SPECIAL_PROPOSAL_PHASE_TOTAL] = 20 minutes;
+      uintConfigs[CONFIG_DRAFT_VOTING_PHASE] = 10 minutes;
+      uintConfigs[CONFIG_VOTE_CLAIMING_DEADLINE] = 10 days;
     }
 
     function mock_set_uint_config(bytes32 _config_name, uint256 _new_value)
