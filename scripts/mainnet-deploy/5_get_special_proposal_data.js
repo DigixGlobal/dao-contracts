2cd18c41034e0ee703caa5774e1e3d5e859cdf92
vicom <vunguyenvyvp@gmail.com>
2019-05-14 09:47:34 +0800

    add more descriptions in printing script

--- scripts/mainnet-deploy/5_get_special_proposal_data.js
+++ scripts/mainnet-deploy/5_get_special_proposal_data.js
@@ -27,8 +27,18 @@ const bN = web3.toBigNumber;
 module.exports = async () => {
   const daoSpecialProposal = await DaoSpecialProposal.deployed();
   const daoConfigsStorage = await DaoConfigsStorage.deployed();
+  console.log('daoSpecialProposal address =', daoSpecialProposal.address);
+  console.log('daoConfigsStorage address =', daoConfigsStorage.address);
 
   const uintConfigs = await daoConfigsStorage.readUintConfigs.call();
+  // console.log("uintConfigs before = ", uintConfigs);
+
+  console.log('Current CONFIG_DRAFT_VOTING_PHASE = ', uintConfigs[indexes.CONFIG_DRAFT_VOTING_PHASE], uintConfigs[indexes.CONFIG_DRAFT_VOTING_PHASE].toNumber()/86400, " days");
+  console.log('Current CONFIG_VOTING_COMMIT_PHASE = ', uintConfigs[indexes.CONFIG_VOTING_COMMIT_PHASE], uintConfigs[indexes.CONFIG_VOTING_COMMIT_PHASE].toNumber()/86400, " days");
+  console.log('Current CONFIG_VOTING_PHASE_TOTAL = ', uintConfigs[indexes.CONFIG_VOTING_PHASE_TOTAL], uintConfigs[indexes.CONFIG_VOTING_PHASE_TOTAL].toNumber()/86400, " days");
+  console.log('Current CONFIG_VOTE_CLAIMING_DEADLINE = ', uintConfigs[indexes.CONFIG_VOTE_CLAIMING_DEADLINE], uintConfigs[indexes.CONFIG_VOTE_CLAIMING_DEADLINE].toNumber()/86400, " days");
+  console.log('Current CONFIG_SPECIAL_PROPOSAL_COMMIT_PHASE = ', uintConfigs[indexes.CONFIG_SPECIAL_PROPOSAL_COMMIT_PHASE], uintConfigs[indexes.CONFIG_SPECIAL_PROPOSAL_COMMIT_PHASE].toNumber()/86400, " days");
+  console.log('Current CONFIG_SPECIAL_PROPOSAL_PHASE_TOTAL = ', uintConfigs[indexes.CONFIG_SPECIAL_PROPOSAL_PHASE_TOTAL], uintConfigs[indexes.CONFIG_SPECIAL_PROPOSAL_PHASE_TOTAL].toNumber()/86400, " days");
 
   assert.ok(!isInvalid(process.env.DOC_IPFS_HASH), 'Please provide the value for DOC_IPFS_HASH');
   assert.ok(!isInvalid(process.env.CONFIG_DRAFT_VOTING_PHASE), 'Please provide the value for CONFIG_DRAFT_VOTING_PHASE');
@@ -44,6 +54,16 @@ module.exports = async () => {
   uintConfigs[indexes.CONFIG_VOTE_CLAIMING_DEADLINE] = bN(process.env.CONFIG_VOTE_CLAIMING_DEADLINE);
   uintConfigs[indexes.CONFIG_SPECIAL_PROPOSAL_COMMIT_PHASE] = bN(process.env.CONFIG_SPECIAL_PROPOSAL_COMMIT_PHASE);
   uintConfigs[indexes.CONFIG_SPECIAL_PROPOSAL_PHASE_TOTAL] = bN(process.env.CONFIG_SPECIAL_PROPOSAL_PHASE_TOTAL);
+  console.log();
+
+  console.log('Proposing CONFIG_DRAFT_VOTING_PHASE = ', uintConfigs[indexes.CONFIG_DRAFT_VOTING_PHASE], uintConfigs[indexes.CONFIG_DRAFT_VOTING_PHASE].toNumber()/86400, " days");
+  console.log('Proposing CONFIG_VOTING_COMMIT_PHASE = ', uintConfigs[indexes.CONFIG_VOTING_COMMIT_PHASE], uintConfigs[indexes.CONFIG_VOTING_COMMIT_PHASE].toNumber()/86400, " days");
+  console.log('Proposing CONFIG_VOTING_PHASE_TOTAL = ', uintConfigs[indexes.CONFIG_VOTING_PHASE_TOTAL], uintConfigs[indexes.CONFIG_VOTING_PHASE_TOTAL].toNumber()/86400, " days");
+  console.log('Proposing CONFIG_VOTE_CLAIMING_DEADLINE = ', uintConfigs[indexes.CONFIG_VOTE_CLAIMING_DEADLINE], uintConfigs[indexes.CONFIG_VOTE_CLAIMING_DEADLINE].toNumber()/86400, " days");
+  console.log('Proposing CONFIG_SPECIAL_PROPOSAL_COMMIT_PHASE = ', uintConfigs[indexes.CONFIG_SPECIAL_PROPOSAL_COMMIT_PHASE], uintConfigs[indexes.CONFIG_SPECIAL_PROPOSAL_COMMIT_PHASE].toNumber()/86400, " days");
+  console.log('Proposing CONFIG_SPECIAL_PROPOSAL_PHASE_TOTAL = ', uintConfigs[indexes.CONFIG_SPECIAL_PROPOSAL_PHASE_TOTAL], uintConfigs[indexes.CONFIG_SPECIAL_PROPOSAL_PHASE_TOTAL].toNumber()/86400, " days");
+  // console.log("uintConfigs after = ", uintConfigs);
+
 
   const encodedHash = encodeHash(process.env.DOC_IPFS_HASH);
   const result = daoSpecialProposal.createSpecialProposal.request(
